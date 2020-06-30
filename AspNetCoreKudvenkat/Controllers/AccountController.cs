59b3d3cebd47640ccc6b9e5274c8652d662e9977
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-06-30 14:55:25 +0300

    Implement login action

--- AspNetCoreKudvenkat/Controllers/AccountController.cs
+++ AspNetCoreKudvenkat/Controllers/AccountController.cs
@@ -43,5 +43,44 @@ namespace AspNetCoreKudvenkat.Controllers
             ModelState.AddModelError(string.Empty, "An error occured.");
             return View(registerViewModel);
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.Email, loginViewModel.Password, loginViewModel.RememberMe, true);
+                if(result.IsLockedOut)
+                {
+                    ModelState.AddModelError(string.Empty, "Too many failed login attempts! Wait for a while to login again");
+                    return View();
+                }
+                if (result.IsNotAllowed)
+                {
+                    ModelState.AddModelError(string.Empty, "This account is suspended. Please contact with the Customer Support.");
+                    return View();
+                }
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else{ return View(); }
+
+            ModelState.AddModelError(string.Empty, "Wrong username or password!");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+        }
     }
 }
