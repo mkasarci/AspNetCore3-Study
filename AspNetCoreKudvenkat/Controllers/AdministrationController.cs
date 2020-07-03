ad3c4b43a06e27f23204dc9ac266332205fb976e
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-07-03 14:09:12 +0300

    Add Edit option to user controller and create view

--- AspNetCoreKudvenkat/Controllers/AdministrationController.cs
+++ AspNetCoreKudvenkat/Controllers/AdministrationController.cs
@@ -1,15 +1,20 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AspNetCoreKudvenkat.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AspNetCoreKudvenkat.Controllers
 {
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _roleManager = roleManager;
         }
 
@@ -17,7 +22,7 @@ namespace AspNetCoreKudvenkat.Controllers
         public async Task<IActionResult> isValidName(string RoleName)
         {
             var identityRole = await _roleManager.FindByNameAsync(RoleName);
-            if(identityRole is null)
+            if (identityRole is null)
             {
                 return Json(true);
             }
@@ -42,17 +47,74 @@ namespace AspNetCoreKudvenkat.Controllers
                     Name = createRoleViewModel.RoleName
                 };
                 var result = await _roleManager.CreateAsync(identityRole);
-                if(result.Succeeded)
+                if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("ListRoles", "Administration");
                 }
-                foreach(var error in result.Errors)
-                {   
+                foreach (var error in result.Errors)
+                {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
             ModelState.AddModelError(string.Empty, "An error occured!");
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            if (id is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role is null)
+            {
+                ModelState.AddModelError(string.Empty, "Couldn't find any role with that ID. Please contact with support team.");
+                return View();
+            }
+
+            var usersInRole = await _userManager.GetUsersInRoleAsync(role.Name);
+            var users = usersInRole.Select(u => u.Email);
+
+            EditRoleViewModel model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+                Users = users.ToList()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel editRoleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(editRoleViewModel.Id);
+                role.Name = editRoleViewModel.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles","Administration");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            ModelState.AddModelError(string.Empty, "An error occured!");
+            return View(editRoleViewModel);
         }
     }
 }
---
+++ AspNetCoreKudvenkat/Models/EditRoleViewModel.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreKudvenkat.Models
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Role Name is required!")] 
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
